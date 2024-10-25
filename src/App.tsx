c0f9b2534b9596e60650ae2e793eaf8efadac009
n1yatsh1n <n1yatsh1n@mail.ru>
2024-10-26 03:05:08 +0600

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <>Test</>;
+  return (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
 }
 
 export default App;
