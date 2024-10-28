64e0f1429bbd6ad086144fe8ad8eea48b8af2418
n1yatsh1n <n1yatsh1n@mail.ru>
2024-10-28 21:47:16 +0600

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Footer/>
     </BrowserRouter>
   );
 }
