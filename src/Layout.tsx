fa294054dde4350cf390d3998cf1a05d7e046460
n1yatsh1n <n1yatsh1n@mail.ru>
2024-11-06 17:43:31 +0600

    add toast notification for requests errors

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { ToastContainer } from "react-toastify";
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <main className="container">
         <Outlet />
       </main>
--- src/api/helpers/axios.ts
+++ src/api/helpers/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import envs from "../../config/environments";
 import md5 from "md5";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const instance = axios.create({
   baseURL: envs.baseApiUrl,
@@ -10,5 +12,27 @@ const instance = axios.create({
     hash: md5(Date.now().toString() + envs.privateKey + envs.apiKey),
   },
 });
+
+instance.interceptors.request.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log("error", error);
+    toast.error(error.message);
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log("error", error);
+    toast.error(error.message);
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
