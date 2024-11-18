6a65de1c3b3d3c6c5e04dcab35c622c7f4263fbe
n1yatsh1n <n1yatsh1n@mail.ru>
2024-11-19 03:09:11 +0600

    add items to favourites page

--- src/pages/favourites/Favourites.tsx
+++ src/pages/favourites/Favourites.tsx
@@ -1,5 +1,36 @@
-const Favourites = () => {
-  return <div>Favourites</div>;
+import React from "react";
+import { IItem } from "../../types/types"; 
+import ItemCard from "../../components/itemCard/ItemCard";
+
+const Favourites: React.FC = () => {
+  const favorites: IItem[] = JSON.parse(
+    localStorage.getItem("favorites") || "[]"
+  );
+
+  return (
+    <div>
+      <div className="titleWrapper">
+        <h1>Favourites</h1>
+        <p>({favorites.length})</p>
+      </div>
+      {favorites.length === 0 ? (
+        <p>У вас нет избранных элементов.</p>
+      ) : (
+        <div className="cardContainer">
+          {favorites.map((item) => (
+            <ItemCard
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              img={item.img}
+              type={item.type}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Favourites;
