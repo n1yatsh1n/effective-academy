7ccdaf3e1be85a1729af2bb8bb7011f1b453d8b6
n1yatsh1n <n1yatsh1n@mail.ru>
2024-11-19 02:59:55 +0600

    adding a temporary check on favorites in item

--- src/components/itemCard/ItemCard.tsx
+++ src/components/itemCard/ItemCard.tsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
 import { IItem } from "../../types/types";
+import { useEffect, useState } from "react";
 import "./ItemCard.css";
+
 const ItemCard: React.FC<IItem> = (props) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+    const exists = favorites.some((item: IItem) => item.id === props.id);
+    setIsFavorite(exists);
+  }, [props.id]);
+
   const handleClick = () => {
     const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
-
     const exists = favorites.some((item: IItem) => item.id === props.id);
 
     if (!exists) {
       favorites.push(props);
       localStorage.setItem("favorites", JSON.stringify(favorites));
+      setIsFavorite(true);
     }
   };
+
   return (
     <Link
       to={
@@ -32,6 +43,11 @@ const ItemCard: React.FC<IItem> = (props) => {
       </div>
       <div className="cardBack">
         <p>{props.description}</p>
+        {isFavorite ? (
+          <p className="favoriteStatus">Этот элемент в избранном</p>
+        ) : (
+          <p className="favoriteStatus">Этот элемент не в избранном</p>
+        )}
         <p className="readMore">Read more...</p>
       </div>
     </Link>
